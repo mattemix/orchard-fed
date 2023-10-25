dbcef576a533309a42e53d15f2e41658d4f44bcd
mattemix <matt@emixgaming.com>
2023-10-25 20:06:46 +0800

    Added meta tags

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import Slider1 from './Slider/Slider1';
 import Header from './Header/Header';
 import { getContainer } from './ioc/IOCSetup';
 import BlogThumbnail from './BlogThumbnail/BlogThumbnail';
+import {Helmet} from "react-helmet";
+import Footer from './Footer/Footer';
 import './App.scss'
 import './assets/css/slick.css'
 import './assets/css/slick-theme.css'
 import './assets/css/slick-animation.css'
 import './assets/css/animate.min.css'
-import Footer from './Footer/Footer';
+
 
 export class App extends Component {
   render() {
@@ -17,6 +19,12 @@ export class App extends Component {
     let content2 = getContainer().getSettingsService().getComponent2();
     return (
       <React.Fragment>
+          <Helmet>
+            <meta charSet="utf-8" />
+            <title lang="en">Orchard FED Exam</title>
+            <meta name="description" content="Orchard Front-end Developers" lang="en" />
+            <meta name="keywords" content="Front-end Developer" lang="en"/>
+        </Helmet>
         <Header />
         <div className='container'>
           <div className='row'>
